759e78d4183241434df339dd7d21e05ab64c43fc
Vladyslav Simak <simakv21@gmail.com>
2023-05-06 09:25:06 +0300

    Fix persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
 import { createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import {rootReducer} from './reducer';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer, enhancer);
+
+export const  store = createStore(persistedReducer, enhancer)
+
+export const persistor = persistStore(store);
